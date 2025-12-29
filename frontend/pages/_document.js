a354858db87b6e0b76665c1389cac20ed0058d10
ajharshal45 <ajharshal45@gmail.com>
2025-12-29 12:58:12 +0530

    Fix PWA icons: Add SVG icons and update manifest

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -7,7 +7,8 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#16a34a" />
         <meta name="description" content="Healthcare guidance, even without internet" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <link rel="icon" href="/icons/icon-192.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.svg" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
